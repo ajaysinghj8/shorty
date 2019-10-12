5c0bae61c03c79d9d215239d55150736e7261258
Ajay Singh <ajaysinghj8@hotmail.com>
2019-10-12 15:59:20 +0530

    extends test cases for create short url

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -24,9 +24,36 @@ describe('API spec', () => {
             .expect(201)
             .expect('Content-Type', /json/)
             .expect(({ body }) => {
-                expect(body.shortcode).to.be.string().and.not.empty();
+                expect(body.shortcode).to.be.eq('example');
             });
 
+    });
+
+    it('should reject the request if url is missing', async () => {
+        await request
+            .post('/shorten')
+            .send({})
+            .set('Accept', 'application/json')
+            .expect(400)
+            .expect('Content-Type', /json/)
+            .expect(({ body }) => {
+                expect(body.message).to.be.eq('url is not present');
+            });
+    });
+
+    it('should reject the request if shortcode format is incorrect', async () => {
+        await request
+            .post('/shorten')
+            .send({
+                "url": "http://example.com",
+                "shortcode": "exa"
+            })
+            .set('Accept', 'application/json')
+            .expect(422)
+            .expect('Content-Type', /json/)
+            .expect(({ body }) => {
+                expect(body.message).to.be.eq('The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{4,}$.');
+            });
     });
 
     after(() => {
